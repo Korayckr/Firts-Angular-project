06711c3ff81cce6b91a51362bd235f092d228f1d
Korayckr <koraycakir1@hotmail.com>
2023-06-19 09:33:09 +0300

    Angular-proje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeroComponent } from './hero/hero.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeroComponent,
+    ContentComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
